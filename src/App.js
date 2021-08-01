f86fd5b48e52ce6b3f0564fd961c91d64f900395
abhaywwe7 <abhaywwe7@gmail.com>
2021-08-01 23:36:27 +0530

    winding up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useReducer } from "react";
 import "./App.css";
-
+import { v4 as uuid } from "uuid";
 const initialNotesState = {
   lastNoteCreated: null,
   totalNotes: 0,
@@ -11,11 +11,20 @@ const notesReducer = (prevState, action) => {
   switch (action.type) {
     case "ADD_NOTE": {
       const newState = {
-        lastNoteCreated: new Date().toTimeString().slice(0, 0),
+        lastNoteCreated: new Date().toTimeString().slice(0, 8),
         totalNotes: prevState.notes.length + 1,
         notes: [...prevState.notes, action.payload],
       };
-      console.log("After adding node", newState);
+      console.log("After adding note", newState);
+      return newState;
+    }
+    case "DELETE_NOTE": {
+      const newState = {
+        ...prevState,
+        notes: prevState.notes.filter((note) => note.id !== action.payload.id),
+        totalNotes: prevState.notes.length - 1,
+      };
+      console.log("After Deleting NOTE: ", newState);
       return newState;
     }
   }
@@ -29,13 +38,31 @@ export function App() {
       return;
     }
     const newNote = {
+      id: uuid(),
       text: noteInput,
+      rotate: Math.floor(Math.random() * 20),
     };
     dispatch({ type: "ADD_NOTE", payload: newNote });
+    setNoteInput("");
+  };
+  const dragOver = (event) => {
+    event.stopPropagation();
+    event.preventDefault();
+  };
+  const dropNote = (event) => {
+    event.target.style.left = `${event.pageX - 50}px`;
+    event.target.style.top = `${event.pageY - 50}px`;
   };
   return (
-    <div className="app">
-      <h1>Sticky Notes</h1>
+    <div className="app" onDragOver={dragOver}>
+      <h1>
+        Sticky Notes({notesState.totalNotes})
+        <span>
+          {notesState.notes.length
+            ? `Last note created: ${notesState.lastNoteCreated}`
+            : " "}
+        </span>
+      </h1>
       <form onSubmit={addNote} className="note-form">
         <textarea
           value={noteInput}
@@ -45,7 +72,34 @@ export function App() {
         <button>Add</button>
       </form>
 
-      {noteInput}
+      {notesState.notes.map((note) => (
+        <div
+          className="note"
+          style={{ transform: `rotate($(note.rotate)deg)` }}
+          draggable="true"
+          onDragEnd={dropNote}
+          key={note.id}
+        >
+          <div
+            onClick={() => dispatch({ type: "DELETE_NOTE", payload: note })}
+            className="close"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </div>
+
+          <pre className="text">{note.text}</pre>
+        </div>
+      ))}
     </div>
   );
 }
